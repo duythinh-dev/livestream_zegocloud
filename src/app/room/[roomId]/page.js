afe972db405fa84e88b6be7dce870397b66f47f2
Duy-Thinh <thinhogg11@gmail.com>
2024-05-20 11:51:48 +0700

    udpate

---
+++ src/app/room/[roomId]/page.js
@@ -0,0 +1,50 @@
+"use client";
+import React, { useRef } from "react";
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+
+export default function Room({ params }) {
+  //   const streamRef = useRef(null);
+  const { roomId } = params;
+  const myMeeting = async (e) => {
+    const appId = 1628765537;
+    const serverSecret = "a208a4bf3df6e21cb911595336bdf125";
+    const userID = Math.floor(Math.random() * 10000) + "";
+    const userName = "userName" + userID;
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appId,
+      serverSecret,
+      roomId,
+      userID,
+      userName
+    );
+
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+    zp.joinRoom({
+      container: e,
+      scenario: {
+        mode: ZegoUIKitPrebuilt.LiveStreaming,
+      },
+      //   sharedLinks: [
+      //     {
+      //       name: "Join as audience",
+      //       url:
+      //         window.location.protocol +
+      //         "//" +
+      //         window.location.host +
+      //         window.location.pathname +
+      //         "?roomID=" +
+      //         roomID +
+      //         "&role=Audience",
+      //     },
+      //   ],
+    });
+  };
+
+  console.log("props", params);
+  return (
+    <div>
+      Room {roomId}
+      <div ref={myMeeting} />
+    </div>
+  );
+}
