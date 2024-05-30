6662db4fb18e8fea20503c0a1158bfd233caac85
Duy-Thinh <thinhogg11@gmail.com>
2024-05-30 16:36:53 +0700

    Update

--- src/app/other/page.js
+++ src/app/other/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useRef, useState } from "react";
-import { generateToken04 } from "../../../../server/zegoServerAssistant";
+import { generateToken04 } from "../../../server/zegoServerAssistant";
 import ReactPlayer from "react-player";
 import moment from "moment";
 import md5 from "md5";
@@ -221,23 +221,179 @@ async function startPlaying({
   }
 }
 
-function LiveStream({ params }) {
-  const randomNumber = Math.floor(Math.random() * 100000);
-  const num = moment().add(3, "hours").unix().toString(16);
-  const md5Hash = md5(
-    num + `/live/${params.roomId}` + process.env.NEXT_PUBLIC_PRIMARY_KEY
-  );
+function LiveStream() {
+  const zgRef = useRef(null);
+
+  const [link, setLink] = useState({
+    streamKey: "",
+    URLlive: "",
+  });
+  const [id, setId] = useState(0);
+  const [state, setState] = useState({
+    userName: "",
+    connectStatus: "DISCONNECTED",
+    connect: {
+      connecting: false,
+      connected: false,
+    },
+    videoCodec: "VP8",
+    checkSystemRequireStatus: "",
+    microphoneDevicesVal: null,
+    cameraDevicesVal: "",
+    cameraCheckStatus: true,
+    microphoneCheckStatus: true,
+    audioCheckStatus: false,
+    playStreamStatus: false,
+    localStream: null,
+    remoteStream: null,
+    isLogin: false,
+    audioDeviceList: [],
+    videoCheckStatus: true,
+    audioCheckStatus: true,
+    numberViewer: 0,
+    sendBitrate: 0,
+    sendFPS: 0,
+    sendPacket: 0,
+    receiveBitrate: 0,
+    receiveFPS: 0,
+    receivePacket: 0,
+    frameWidth: 0,
+    frameHeight: 0,
+  });
+  const zg = zgRef.current;
+  const userID = Math.floor(Math.random() * 10000) + "";
+
+  const userNameJoin = "state.userName" + "_" + userID;
+  const effectiveTimeInSeconds = 60 * 60 * 24 * 30;
+  const appID = process.env.NEXT_PUBLIC_ZEGO_APP_ID * 1;
+  const server = process.env.NEXT_PUBLIC_ZEGO_SERVER_ID;
+  const handleJoinRoom = async () => {
+    const token = generateToken04(
+      appID,
+      userNameJoin,
+      server,
+      effectiveTimeInSeconds,
+      ""
+    );
+
+    await loginRoomOption({
+      zg: zg,
+      roomID: id,
+      token,
+      userID: userNameJoin,
+      userName: "state.userName",
+    });
+  };
+  const handleChangeValueState = (val) => {
+    setState({
+      ...state,
+      ...val,
+    });
+  };
+
+  useEffect(() => {
+    const randomNumber = Math.floor(Math.random() * 100000);
+
+    const num = moment().add(3, "hours").unix().toString(16);
+    const md5Hash = md5(
+      num + `/live/${randomNumber}` + process.env.NEXT_PUBLIC_PRIMARY_KEY
+    );
+    import("zego-express-engine-webrtc").then(({ ZegoExpressEngine }) => {
+      const zg = new ZegoExpressEngine(appID, server);
+      zgRef.current = zg;
+
+      CheckSystemRequire(zg, handleChangeValueState);
+      zg.on("roomStateUpdate", (roomId, state) => {
+        console.log("Trạng thái: ", roomId, state);
+        if (state === "CONNECTING") {
+          setState((prev) => ({
+            ...prev,
+            connect: {
+              connecting: true,
+              connected: false,
+            },
+            connectStatus: state,
+          }));
+        }
+        if (state === "CONNECTED") {
+          setState((prev) => ({
+            ...prev,
+            connect: {
+              connecting: false,
+              connected: true,
+            },
+            connectStatus: state,
+          }));
+        }
+
+        if (state === "DISCONNECTED") {
+          setState((prev) => ({
+            ...prev,
+            connectStatus: state,
+          }));
+        }
+      });
+
+      zg.on("publisherStateUpdate", (result) => {
+        if (result.state === "PUBLISHING") {
+          // $('#pushlishInfo-id').text(result.streamID)
+        } else if (result.state === "NO_PUBLISH") {
+          // $('#pushlishInfo-id').text('')
+        }
+      });
+
+      zg.on("playerStateUpdate", (result) => {
+        if (result.state === "PLAYING") {
+          // $('#playInfo-id').text(result.streamID)
+        } else if (result.state === "NO_PLAY") {
+          // $('#playInfo-id').text('')
+        }
+      });
+
+      zg.on("publishQualityUpdate", (streamId, stats) => {
+        console.log("Stream ID: ", streamId);
+        setState((prev) => ({
+          ...prev,
+          frameWidth: stats.video.frameWidth,
+          frameHeight: stats.video.frameHeight,
+          videoBitrate: stats.video.videoBitrate,
+          videoFPS: stats.video.videoFPS,
+          videoPacketsLostRate: stats.video.videoPacketsLostRate,
+        }));
+      });
+      zg.on("IMRecvBroadcastMessage", (roomID, messageList) => {
+        setChats((prev) => [
+          ...prev,
+          ...messageList.map((item) => ({
+            message: item.message,
+            name: item.fromUser.userName,
+          })),
+        ]);
+      });
+      zg.on("playQualityUpdate", (streamId, stats) => {
+        setState((prev) => ({
+          ...prev,
+          frameWidth: stats.video.frameWidth,
+          frameHeight: stats.video.frameHeight,
+          receiveBitrate: stats.video.videoBitrate,
+          receiveFPS: stats.video.videoFPS,
+          receivePacket: stats.video.videoPacketsLostRate.toFixed(1),
+        }));
+      });
+      handleJoinRoom();
+      setLink({
+        streamKey: `${randomNumber}?wsSecret=${md5Hash}&wsABStime=${num}`,
+        URLlive: `http://play-ws1.copbeo.com/live/${randomNumber}/playlist.m3u8`,
+      });
+    });
+  }, []);
+
   return (
     <div className="p-4">
       <p>Tạo stream với phần mềm thứ 3 ( OBS, ... )</p>
       <div>Server : {process.env.NEXT_PUBLIC_URL_SERVER}</div>
-      <div>
-        Stream key: {params.roomId}?wsSecret=${md5Hash}&wsABStime=${num}
-      </div>
-      <div>
-        URL live:{" "}
-        {`http://play-ws1.copbeo.com/live/${params.roomId}/playlist.m3u8`}
-      </div>
+      <div>Stream key:{link.streamKey}</div>
+      <div>URL live: {link.URLlive}</div>
     </div>
   );
 }
